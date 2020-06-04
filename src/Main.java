33ad55144da5f9f45c3df64826ea2cad967720cc
MelodyC117 <mchen1171@gmail.com>
2020-06-03 22:00:39 -0400

    first complete version, troubleshooting ...

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,37 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String [] args) {
-        Simulation sim = new Simulation();
+    public static void main(String [] args) throws Exception {
 
+        double totalCost = 0;
+        Simulation sim = new Simulation();
+        ArrayList<Item> cargoListP1 = new ArrayList<>();
+        ArrayList<Item> cargoListP2 = new ArrayList<>();
+        ArrayList<Rocket> U1P1 = new ArrayList<>();
+        ArrayList<Rocket> U1P2 = new ArrayList<>();
+        ArrayList<Rocket> U2P1 = new ArrayList<>();
+        ArrayList<Rocket> U2P2 = new ArrayList<>();
+
+        // U1 - load all from P1
+        cargoListP1 = sim.loadItems(cargoListP1, 1);
+        U1P1 = sim.loadU1(cargoListP1);
+        totalCost = sim.runSimulation(U1P1);
+        System.out.println("Total budget for Phase 1 with U1 is " + totalCost);
+
+        // U1 - load all from P2
+        cargoListP2 = sim.loadItems(cargoListP2, 2);
+        U1P2 = sim.loadU1(cargoListP2);
+        totalCost = sim.runSimulation(U1P2);
+        System.out.println("Total budget for Phase 1 with U1 is " + totalCost);
+
+        // U2 - load all from P1
+        U2P1 = sim.loadU2(cargoListP1);
+        totalCost = sim.runSimulation(U2P1);
+        System.out.println("Total budget for Phase 1 with U1 is " + totalCost);
+
+        // U2 - load all from P1
+        U2P2 = sim.loadU2(cargoListP2);
+        totalCost = sim.runSimulation(U2P2);
+        System.out.println("Total budget for Phase 1 with U1 is " + totalCost);
     }
 }
--- src/Simulation.java
+++ src/Simulation.java
@@ -10,7 +10,7 @@ public class Simulation {
      * Each line in the text file consists of the item name followed by = then its weigh in kg.
      */
     public ArrayList<Item> loadItems(ArrayList<Item> cargoList, int phase) throws Exception {
-        File file = new File((phase == 1) ? "P1.txt" : "P2.txt");
+        File file = new File((phase == 1) ? "src/P1.txt" : "src/P2.txt");
         Scanner scan = new Scanner(file);
 
         while (scan.hasNextLine()) {
@@ -28,17 +28,85 @@ public class Simulation {
      * It first tries to fill up 1 rocket with as many items as possible before creating a new rocket object and
      * filling that one until all items are loaded. The method then returns the ArrayList of those U1 rockets that are fully loaded.
      */
-    public ArrayList<U1> loadU1(ArrayList<Item> cargoList) {
-        ArrayList<Item> U1Item = new ArrayList<Item>();
+    public ArrayList<Rocket> loadU1(ArrayList<Item> cargoList) {
+        ArrayList<Rocket> U1Item = new ArrayList<>();
+        U1 U1Rocket = new U1();
+        int counter = 1;
 
-        int i = 0;
-        while (i < cargoList.size()){
-            U1 U1Rocket = new U1();
-            while (U1Rocket.currentWeight < U1Rocket.maxWeight) {
-
+        for (Item cargo : cargoList) {
+            if (U1Rocket.canCarry(cargo)) {
+                U1Rocket.carry(cargo);
+                System.out.println("Loading ... " + cargo.weight + " kg");
+                System.out.println("Current Weight: " + U1Rocket.currentWeight + " kg");
+            }
+            else {
+                U1Item.add(U1Rocket);
+                U1Rocket = new U1();
+                counter++;
+                U1Rocket.carry(cargo);
+                System.out.println("Creating New Rocket ... ");
+                System.out.println("Loading ... " + cargo.weight + " kg");
+                System.out.println("Current Weight: " + U1Rocket.currentWeight + " kg");
             }
         }
+        U1Item.add(U1Rocket);
+        System.out.println("Total Rockets for U1 created: " + counter);
         return U1Item;
+    };
+
+    public ArrayList<Rocket> loadU2(ArrayList<Item> cargoList) {
+        ArrayList<Rocket> U2Item = new ArrayList<>();
+        U2 U2Rocket = new U2();
+        int counter = 1;
+
+        for (Item cargo : cargoList) {
+            if (U2Rocket.canCarry(cargo)) {
+                U2Rocket.carry(cargo);
+                System.out.println("Loading ... " + cargo.weight + " kg");
+                System.out.println("Current Weight: " + U2Rocket.currentWeight + " kg");
+            }
+            else {
+                U2Item.add(U2Rocket);
+                U2Rocket = new U2();
+                U2Rocket.carry(cargo);
+                counter++;
+                U2Rocket.carry(cargo);
+                System.out.println("Creating New Rocket ... ");
+                System.out.println("Loading ... " + cargo.weight + " kg");
+                System.out.println("Current Weight: " + U2Rocket.currentWeight + " kg");
+            }
+        }
+        U2Item.add(U2Rocket);
+        System.out.println("Total Rockets for U2 created: " + counter);
+        return U2Item;
+    };
+
+    /**
+     * this method takes an ArrayList of Rockets and calls launch and land methods for each of the rockets in the ArrayList.
+     * Every time a rocket explodes or crashes (i.e if launch or land return false) it will have to send that rocket again.
+     * All while keeping track of the total budget required to send each rocket safely to Mars. runSimulation then returns
+     * the total budget required to send all rockets (including the crashed ones).
+     */
+    public int runSimulation(ArrayList<Rocket> rockets) {
+
+        boolean launchSuccess;
+        boolean landSuccess;
+        int totalBudget = 0;
+        for (Rocket rocket : rockets) {
+            launchSuccess = rocket.isLaunch();
+            landSuccess = rocket.isLand();
+            System.out.println("Launch Success: " + launchSuccess);
+            System.out.println("Land Success: " + landSuccess);
+            while (!launchSuccess || !landSuccess) {
+                totalBudget += rocket.cost;
+                launchSuccess = rocket.isLaunch();
+                landSuccess = rocket.isLand();
+                System.out.println("Launch Success: " + launchSuccess);
+                System.out.println("Land Success: " + landSuccess);
+                System.out.println("Total Cost: " + totalBudget);
+            }
+        }
+        return totalBudget;
     };
 
 }
